190d1d7c663e52223b8997a4ab37d10b06c4b2cd
Bloodiko <bastianjesuiter@gmail.com>
2022-09-17 17:13:22 +0200

    implement base for Difficulty

--- src/components/Difficulty.tsx
+++ src/components/Difficulty.tsx
@@ -1,10 +1,19 @@
 //component for difficulty
-import React from 'react';
 
 const Difficulty = () => {
     return (
         <div>
             <h1>Difficulty</h1>
+
+            <h3>Select Difficulty for the new Game:</h3>
+            <br />
+            <div className="diffDiv">
+                <button className="diffBtn" onClick={() => { }}>Easy</button>
+                <button className="diffBtn" onClick={() => { }}>Medium</button>
+                <button className="diffBtn" onClick={() => { }}>Hard</button>
+                <button className="diffBtn" onClick={() => { }}>Expert</button>
+                <button className="diffBtn" onClick={() => { }}>Random</button>
+            </div>
         </div>
     );
 }
