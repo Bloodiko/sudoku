5cb3b49426605771d44f8bf3c16bebf70545fd57
bloodiko <bastian.jesuiter@gmail.com>
2022-09-19 10:46:45 +0200

    add useEffekt
    no more unlimited rerenders.
    
    This is absolutely NOT good. but i dont know better

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,9 +1,9 @@
 //component for game
-import React from 'react';
+import React, { useMemo } from 'react';
 import { DifficultyContext } from './DifficultyContext';
 import { useContext } from 'react';
-import { useEffect } from 'react';
 import diffEnum from './difficultyEnum';
+import { useEffect } from 'react';
 
 import { Difficulty } from 'sudoku-gen/dist/types/difficulty.type';
 import { Sudoku } from 'sudoku-gen/dist/types/sudoku.type';
@@ -12,37 +12,56 @@ import { useState } from 'react';
 
 import { getSudoku } from 'sudoku-gen'
 
+//convert difficulty enum to sudoku-gen difficulty -> is there an easier way?
+const genDiff = (diff: diffEnum): Difficulty | undefined => {
+    switch (diff) {
+        case diffEnum.EASY:
+            return "easy";
+        case diffEnum.MEDIUM:
+            return "medium";
+        case diffEnum.HARD:
+            return "hard";
+        case diffEnum.EXPERT:
+            return "expert";
+        default:
+            return undefined;
+    }
+}
+
 const Game = () => {
     const { gamedifficulty, setDifficulty } = useContext(DifficultyContext);
 
     const [game, setGame] = useState(undefined as Sudoku | undefined);
+    console.log('rerender game')
+
+
+    // { game: game, setDifficulty: setDifficulty }
+    const combinedGameDiff = useMemo(() => {
+        return { game: game, setDifficulty: setDifficulty, gamedifficulty: gamedifficulty }
+    }, [game, setDifficulty, gamedifficulty]);
+
+    const combinedDiffSetGame = useMemo(() => {
+        return { difficulty: gamedifficulty, setGame: setGame, game: game }
+    }, [gamedifficulty, setGame, game]);
+
 
     useEffect(() => {
-        game && setDifficulty(diffEnum.NONE);
-    }, [game, setDifficulty]);
-
-    //convert difficulty enum to sudoku-gen difficulty -> is there an easier way?
-    const genDiff = (diff: diffEnum): Difficulty => {
-        switch (diff) {
-            case diffEnum.EASY:
-                return "easy";
-            case diffEnum.MEDIUM:
-                return "medium";
-            case diffEnum.HARD:
-                return "hard";
-            case diffEnum.EXPERT:
-                return "expert";
-            default:
-                return "easy";
+        if (combinedGameDiff.gamedifficulty !== diffEnum.NONE) {
+            return;
         }
-    }
+        console.log('game exists - reset difficulty')
+        combinedGameDiff.setDifficulty(diffEnum.NONE)
+    }, [combinedGameDiff])
 
-    if (gamedifficulty !== diffEnum.NONE) {
+    useEffect(() => {
+        if (combinedDiffSetGame.game) {
+            return;
+        }
+        console.log('game does not exist - generate sudoku')
+        setGame(getSudoku(genDiff(combinedDiffSetGame.difficulty)))
+    }, [combinedDiffSetGame])
 
-        setGame(getSudoku(
-            gamedifficulty === diffEnum.RANDOM ? undefined : genDiff(gamedifficulty)
-        ));
-    }
+
     return (
         <div>
             <h1>Game</h1>
