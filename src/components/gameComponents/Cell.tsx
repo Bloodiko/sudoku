f243d48fa99e3e30ed1c37902065ce1cc6c37328
bloodiko <bastian.jesuiter@gmail.com>
2022-09-20 09:52:20 +0200

    Formatting Changes for Cell

--- src/components/gameComponents/Cell.tsx
+++ src/components/gameComponents/Cell.tsx
@@ -2,9 +2,8 @@ import Candidates from "./Candidates"
 
 const Cell = (props: cellProps) => {
 
-
     return (
-        <div className={'cell' + props.locked && 'locked'}>
+        <div className={'cell ' + (props.locked ? ' locked' : '')}>
             {props.value ? <div className="cellValue">{props.value}</div> : <Candidates candidates={props.candidates} />}
         </div>
     )
