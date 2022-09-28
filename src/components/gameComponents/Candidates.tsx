14bec4cb51abf8af63d693af7104b44102dc3bcb
bloodiko <bastian.jesuiter@gmail.com>
2022-09-28 11:28:58 +0200

    change cell Candates

--- src/components/gameComponents/Candidates.tsx
+++ src/components/gameComponents/Candidates.tsx
@@ -7,17 +7,13 @@ const Candidates = (props: candidateProps) => {
 
     return (
         <>
-            <div className="candidateRow">
+            <div className="cellCandidatesWrapper">
                 <div className="canCell">{candidates[props.cell].one ? '1' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].two ? '2' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].three ? '3' : ' '}</div>
-            </div>
-            <div className="candidateRow">
                 <div className="canCell">{candidates[props.cell].four ? '4' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].five ? '5' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].six ? '6' : ' '}</div>
-            </div>
-            <div className="candidateRow">
                 <div className="canCell">{candidates[props.cell].seven ? '7' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].eight ? '8' : ' '}</div>
                 <div className="canCell">{candidates[props.cell].nine ? '9' : ' '}</div>
