59748a53724c0e04ebe50fa8b50d29da31639754
bloodiko <bastian.jesuiter@gmail.com>
2022-09-30 10:48:20 +0200

    add select Cell

--- src/components/gameComponents/Cell.tsx
+++ src/components/gameComponents/Cell.tsx
@@ -2,16 +2,24 @@ import { useContext } from "react";
 import Candidates from "./Candidates";
 import { GameContext } from "./GameContext";
 
+import { Actions } from "../../types/GameDataReducerType";
+
 const Cell = (props: cellProps) => {
 
     // use game context
     const { gamedata, dispatch } = useContext(GameContext);
     const value = gamedata.currentGameState[props.cell];
 
-    const classes = "cell " + (props.locked ? "locked" : "editable") + " row" + props.row + " col" + props.col + " cube" + props.cube;
+    const classes = "cell " + (props.locked ? "locked" : "editable") + " row" + props.row + " col" + props.col + " cube" + props.cube + (gamedata.selectedCell === props.cell ? " selected" : "");
+
+    const onClick = () => {
+        if (!props.locked) {
+            dispatch({ type: Actions.selectCell, payload: props.cell })
+        }
+    }
 
     return (
-        <div className={classes}>
+        <div className={classes} onClick={onClick}>
             {
                 value !== "-" ? value : <Candidates cell={props.cell} />
             }
